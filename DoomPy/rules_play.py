1b4fe4cc40a3b2a6a7388ddcd6844ecb49c3bdf4
Adam Zabicki <azabicki@posteo.de>
2023-12-18 15:59:26 +0100

    added some more play requirements

--- DoomPy/rules_play.py
+++ DoomPy/rules_play.py
@@ -33,11 +33,25 @@ def check_requirement(trait_idx, p):
                           '>>> play <<< ERROR - for CITRUS to play, a trait with a negative face value needed')
                 return True
 
+        case 'Delicious':
+            # return, if there are no colorless traits in trait pile
+            if sum(['colorless' in color.lower() for color in status_df.loc[tp].color.tolist()]) == 0:
+                write_log(['*'],
+                          '>>> play <<< ERROR - for DELICIOUS to play, 1 or more colorless traits in trait pile needed')
+                return True
+
         case 'Epic':
             # return, if there is another dominant in trait pile
             if sum([1 for t in tp if traits_df.loc[t].dominant == 1]) == 1:
                 write_log(['*'],
                           '>>> play <<< ERROR - already 1 dominant in trait pile - EPIC not allowed')
+                return True
+
+        case 'Fronds':
+            # return, if there are no traits in trait pile
+            if len(tp) == 0:
+                write_log(['*'],
+                          '>>> play <<< ERROR - for FRONDS JAW to play, at least one trait in trait pile needed')
                 return True
 
         case 'Heroic':
@@ -56,11 +70,27 @@ def check_requirement(trait_idx, p):
                           '>>> play <<< ERROR - for METAMORPHOSIS to play, 3 or more traits with face value 1 or higher in trait pile needed')  # noqa: E501
                 return True
 
+        case 'Morality':
+            # return, if there are no positive face value traits in trait pile
+            if sum(face > 0
+                   for face in status_df.loc[tp].face.tolist()
+                   if not isinstance(face, str)) == 0:
+                write_log(['*'],
+                          '>>> play <<< ERROR - for MORALITY to play, a trait with a positive face value needed')
+                return True
+
         case 'Opposable Thumbs':
             # return, if there is another dominant in trait pile
             if sum([1 for t in tp if traits_df.loc[t].dominant == 1]) == 0:
                 write_log(['*'],
                           '>>> play <<< ERROR - no dominant in trait pile - OPPOSABLE THUMBS not allowed')
+                return True
+
+        case 'Retractable Claws':
+            # return, if no red trait in trait pile
+            if sum('red' in color.lower() for color in status_df.loc[tp].color.tolist()) == 0:
+                write_log(['*'],
+                          '>>> play <<< ERROR - to play RETRACTABLE CLAWS, you need at least one red trait in trait pile')
                 return True
 
         case 'Silk':

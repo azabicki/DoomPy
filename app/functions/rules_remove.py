f8518e7585f5c52ba36114712839b692ca5034f2
Adam Zabicki <azabicki@posteo.de>
2024-06-19 21:44:01 +0200

    [app] add rules_remove

---
+++ app/functions/rules_remove.py
@@ -0,0 +1,73 @@
+import streamlit as st
+
+
+def check_trait(trait_idx: int, from_: int, where_to: str) -> str | None:
+    # shorten df's
+    status_df = st.session_state.df["status_df"]
+
+    # get trait name
+    trait = status_df.loc[trait_idx].trait
+
+    # match trait
+    match trait:
+        case "Ironwood":
+            # take away protection from traits which were protected by Ironwood
+            cur_effects = status_df.loc[trait_idx].effects
+            if cur_effects != "none":
+                protected = [int(i) for i in cur_effects.split()]
+                for t in protected:
+                    status_df.loc[t, "no_remove"] = False
+
+                print(
+                    ["*"],
+                    ">>> trait pile <<< '{}' (id:{}) does not protect anymore".format(
+                        trait, trait_idx
+                    ),
+                )
+
+            # save df's
+            st.session_state.df["status_df"] = status_df
+
+            return None
+
+        case "Meek":
+            # take away protection from traits which were protected by Meek
+            cur_effects = status_df.loc[trait_idx].effects
+            if cur_effects != "none":
+                protected = [int(i) for i in cur_effects.split()]
+                for t in protected:
+                    status_df.loc[t, "no_remove"] = False
+
+                print(
+                    ["*"],
+                    ">>> trait pile <<< '{}' (id:{}) does not protect anymore".format(
+                        trait, trait_idx
+                    ),
+                )
+
+            # save df's
+            st.session_state.df["status_df"] = status_df
+
+            return None
+
+        case "Spores":
+            # save player id for each time spores is discarded! this string,
+            # which becomes longer every time spores is discarded, is checked
+            # during update_genes()
+            if not status_df.loc[trait_idx].inactive and where_to == "discard":
+                cur_effects = status_df.loc[trait_idx].effects
+                if cur_effects == "none":
+                    new_effects = str(from_)
+                else:
+                    new_effects = cur_effects + "_" + str(from_)
+
+                # update cur_effect
+                status_df.loc[trait_idx, "effects"] = new_effects
+
+            # save df's
+            st.session_state.df["status_df"] = status_df
+
+            return "keep_trait_effect"
+
+        case _:
+            return None
